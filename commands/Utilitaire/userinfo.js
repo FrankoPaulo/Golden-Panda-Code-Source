1330d55b6b7c0e512a75f4eaec028d8cbad020e1
FrankoPaulo <83468880+FrankoPaulo@users.noreply.github.com>
2021-05-25 21:19:08 +0200

    Modif UserInfo

--- commands/Utilitaire/userinfo.js
+++ commands/Utilitaire/userinfo.js
@@ -2,24 +2,29 @@ const { MessageEmbed } = require("discord.js");
 const moment = require("moment");
 
 module.exports.run = (client, message, args, userInfo) => {
+
+  //TODO: Ajouter/Changer/Supprimer des données de user
+
    let member = message.member;
    if (args[0]) member = message.mentions.members.first() ? message.mentions.members.first() : message.guild.members.cache.get(args[0]) ? message.guild.members.cache.get(args[0]) : null;
    let user = member.user;
+   moment.locale('fr');
   
   const embed = new MessageEmbed()
-    .setTitle(`**${member.user.tag}**`, message.author.avatarURL())
+    .setTitle(`**Informations sur ${member.user.tag}**`)
+    .setAuthor(`Commande invoqué par ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, size: 128 }))
     .setColor('B4E0E0')
-    .setThumbnail(user.displayAvatarURL())
+    .setThumbnail(user.displayAvatarURL({ dynamic: true }))
     .addFields(
       {name: "📄 **Surnom**", value: `${!member.nickname ? 'Aucun Surnom' : `${member.nickname}`}`, inline: true},
       {name: "🤖 **Bot**", value: `${user.bot ? 'Cet Utilisateur est un Bot' : 'Cet Utilisateur n\'est pas un Bot'}`, inline: true},
       {name: "🆔 **ID**", value: `${member.id}`, inline: true},
-      {name: "🕐 **Compte Crée le**", value: `${moment(user.createdAt).format('\`DD/MM/YYYY | hh:mm\`')}`, inline: true},
-      {name: "🕐 **A Rejoins le**", value: `${moment(member.joinedAt).format('\`DD/MM/YYYY | hh:mm\`')}`, inline: true},
+      {name: "🕐 **Compte Crée le**", value: `${moment(user.createdAt).format('LLL')} \`(${moment(user.createdAt).startOf('day').fromNow()})\``, inline: true},
+      {name: "🕐 **A Rejoins le**", value: `${moment(member.joinedAt).format('LLL')} \`(${moment(member.joinedAt).startOf('day').fromNow()})\``, inline: true},
       {name: '\u200b', value: `\u200b`, inline: true},
       {name: "❗ **Nombre de Warn**", value: `${userInfo.warnlevel} Warns sur le serveur`, inline: true},
       {name: "✨ **Boost Serveur**", value: `${member.premiumSince ? `Depuis le ${member.premiumSinceTimestamp}` : "N'a pas Boost le Serveur"}`, inline: true},
-      {name: "🎭 **Rôles**", value: `${member.roles.cache.map(roles => `\`${roles.name}\``).join(', ')}`, inline: false},
+      {name: "🎭 **Rôles**", value: `${member.roles.cache.map(roles => `<@&${roles.id}>`).join(', ')}`, inline: false},
      
     )
 
