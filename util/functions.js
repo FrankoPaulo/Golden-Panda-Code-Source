9039ae430944c2d8796e5eaa5dc6e2f2ef0e13ba
FrankoPaulo <83468880+FrankoPaulo@users.noreply.github.com>
2021-05-25 21:20:16 +0200

    Fix du Setup + Modif fonction updateUserInfo

--- util/functions.js
+++ util/functions.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-const { Guild, attributs, battle, experience, inventories, money, user } = require("../models/index");
+const { Guild } = require("../models/index");
 
 module.exports = async client => {
+
   client.createGuild = async guild => {
     const merged = Object.assign({ _id: mongoose.Types.ObjectId() }, guild)
     const createGuild = await new Guild(merged);
@@ -30,27 +31,8 @@ module.exports = async client => {
     return data.users[position];
   };
 
-  /*A Faire plus tard (Refonte BDD)*/
-  // client.createUserStats = async member => {
-  //   const merged = Object.assign({ _id: mongoose.Types.ObjectId() }, member)
-  //   const createAttributs = await new attributs(merged);
-  //   createAttributs.save()
-  //   const createBattles = await new battle(merged);
-  //   createBattles.save()
-  //   const createExperience = await new experience(merged);
-  //   createExperience.save()
-  //   const createInventories = await new inventories(merged);
-  //   createInventories.save()
-  //   const createMoney = await new money(merged);
-  //   createMoney.save()
-  //   const createUser = await new user(merged);
-  //   createUser.save() 
-  // };
+  //TODO: Refaire la DataBase ainsi que le stockage de données - Adapter le code au multi-server
 
-  // client.getUserAttributs = async member => {
-  //   const data = await attributs.findOne({ attributs: member.id });
-  //   if (data) return data;
-  // };
 
 
 
@@ -86,88 +68,20 @@ module.exports = async client => {
         }
       }
     )
-      // attributs.updateOne(
-      //   { guildID: guild.id },
-      //   {
-      //     $push: {
-      //       id: member.id,
-      //       attributs: {}
-      //     }
-      //   }
-      // ),
-      // battle.updateOne(
-      //   { GuildID: guild.id },
-      //   {
-      //     $push: {
-      //       id: member.id,
-      //       kills: 0,
-      //       deaths: 0,
-      //       isBattle: false
-      //     }
-      //   }
-      // ),
-      // experience.updateOne(
-      //   { guildID: guild.id },
-      //   {
-      //     $push: {
-      //       id: member.id,
-      //       experience: 0,
-      //       level: 1
-      //     }
-      //   }
-      // ),
-      // inventories.updateOne(
-      //   { guildID: guild.id },
-      //   {
-      //     $push: {
-      //       id: member.id,
-      //       inventaire: [],
-      //       equipement: {
-      //         "Mh": "Aucun",
-      //         "Oh": "Aucun",
-      //         "Helmet": "Aucun",
-      //         "Chest": "Aucun",
-      //         "Gloves": "Aucun",
-      //         "Legs": "Aucun",
-      //         "Boots": "Aucun"
-      //       }
-      //     }
-      //   }
-      // ),
-      // money.updateOne(
-      //   { guildID: guild.id },
-      //   {
-      //     $push: {
-      //       id: member.id,
-      //       money: 0,
-      //       eventcoins: 0
-      //     }
-      //   }
-      // ),
-      // user.updateOne(
-      //   { guildID: guild.id },
-      //   {
-      //     $push: {
-      //       id: member.id,
-      //       class: "",
-      //       zone: "Spawn",
-      //       warnlevel: 0
-      //     }
-      //   }
-      // )
       .then(console.log("Nouvelle Utilisateur Enregistré !"))
-};
+  };
 
-client.updateUserInfo = (member, options = {}) => {
-  Guild.updateOne(
-    { "users.id": member.id },
-    { $set: options }
-  ).then(c => console.log(c))
-};
+  client.updateUserInfo = (member, options = {}) => {
+    const guild = Guild.findOne({ guildID: member.guild.id })
+    guild.updateOne(
+      { "users.id": member.id },
+      { $set: options }
+    ).then(c => console.log(c))
+  };
 
-client.createMissingInfoOnUser = (member, missingInfo = {}) => {
-  Guild.updateOne({ "users.id": member.id }, { $set: missingInfo }).then();
-};
+  client.createMissingInfoOnUser = (member, missingInfo, guild = {Guild}) => {
+    guild.updateOne({ "users.id": member.id }, { $set: missingInfo }).then();
+  };
 
 
 };
