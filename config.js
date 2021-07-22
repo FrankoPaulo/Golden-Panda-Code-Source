316d1b976b08d40176a6e247e594f16e538f9191
FrankoPaulo <83468880+FrankoPaulo@users.noreply.github.com>
2021-07-22 15:03:51 +0200

    Nouvelle Config (sera utilisé plus tard)

--- config.js
+++ config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  PREFIX: ".",
-  TOKEN: "TOKEN",
-  DBCONNECTION: "DB CONNECTION",
+  NAME: "GP Dev",
+  TOKEN: "ODE0ODQzNjMzMDU2NDgxMzMw.YDjwRQ.NjkjvP4Z0_RBmII0TL7RoiVKquI",
+  DBCONNECTION: "mongodb+srv://GPConnection:NONauFromage22@gptestbdd.zyij6.mongodb.net/GPTestDataTest?retryWrites=true&w=majority",
+  DEFAULTSETTINGS: {
+    prefix: ".",
+    logChannel: "Aucun",
+    welcomeMessage: "Aucun"
+  }
 }
+
+// ODE0ODQzNjMzMDU2NDgxMzMw.YDjwRQ.NjkjvP4Z0_RBmII0TL7RoiVKquI (GPTest)
+// ODAzNTcwNDI1MjcwNzYzNTQx.YA_tRw.aOijs_Lqrq0Qne-JmmATVU24dds (Bot de Code)
+// NzY0MTgwOTg4MDgyNjUxMTM4.X4ChAQ.2N49BVDQbP2WCgypUZe7fxcZ4Zc (Hébergeur Offline)
