ec5dd039d7b5cfade362b0f600d5f4c590355584
FrankoPaulo <83468880+FrankoPaulo@users.noreply.github.com>
2021-07-22 15:43:50 +0200

    Ajout config prefix

--- commands/Admin/config.js
+++ commands/Admin/config.js
@@ -1,33 +1,37 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
+  //TODO: Refaire la commande et les configs + refaire la database pour les configs
+
+
+  const settings = await client.getGuild(message.guild);
   const getSetting = args[0];
   const newSetting = args.slice(1).join(" ");
 
   const error = new MessageEmbed()
-  .setTitle("<:Warning:766722345280208927> **Erreur de Permission**")
-  .setColor("#870606")
-  .setDescription(`**Vous avez pas les permissions requises pour effectuer la commande \`Config\`**\n\n**Niveau/Type de Permission Requises :** Niveau 5 (Propriétaire)\n**Permission Requise :** \`ADMINISTRATOR\``)
-  .setTimestamp()
-  .setFooter(message.author.username, message.author.avatarURL())
+    .setTitle("<:Warning:840521136701833226> **Erreur de Permission**")
+    .setColor("#870606")
+    .setDescription(`**Vous avez pas les permissions requises pour effectuer la commande \`Config\`**\n\n**Niveau/Type de Permission Requises :** Niveau 5 (Propriétaire)\n**Permission Requise :** \`ADMINISTRATOR\``)
+    .setTimestamp()
+    .setFooter(message.author.username, message.author.avatarURL())
 
   if (message.author.id !== "550251727698264064") return message.channel.send(error)
 
-  switch(getSetting) {
+  switch (getSetting) {
     case "prefix": {
       if (newSetting) {
         await client.updateGuild(message.guild, { prefix: newSetting });
-        return message.channel.send(`Le Prefix a été mis à jour : \n\n\`Ancien Prefix : ${client.config.PREFIX}\` -> \`Nouveau Prefix : ${newSetting}\``);
+        return message.channel.send(`Le Prefix a été mis à jour : \n\n\`Ancien Prefix : ${settings.prefix}\` -> \`Nouveau Prefix : ${newSetting}\``);
       }
-      message.channel.send(`Prefix Actuel : \`${client.config.PREFIX}\``);
+      message.channel.send(`Prefix Actuel : \`${settings.prefix}\``);
       break;
-  }
+    }
+  };
 }
-  
-};
+
 module.exports.help = {
-  name: "eval",
-  aliases: ['eval', 'ev'],
+  name: "config",
+  aliases: ['config'],
   category: 'admin',
   description: "Permet de configurer le Bot",
   cooldown: 10,
@@ -35,7 +39,7 @@ module.exports.help = {
   isUserAdmin: false,
   permissions: true,
   permission: 'Niveau 5 (Propriétaire)',
-  type: 'ADMINISTRATOR',
+  permissionType: 'ADMINISTRATOR',
   args: true,
   profile: false
 };
